e97db7149f2c3604e308dad14af80351e696d02a
Bruno Pires Ultramari <58628700+bruno-ultramari@users.noreply.github.com>
2021-01-13 22:32:33 -0300

    Pontos-naofuncional

---
+++ src/Application/Repository/IDetranVerificadorPontosRepository.cs
@@ -0,0 +1,12 @@
+using DesignPatternSamples.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternSamples.Application.Repository
+{
+    public interface IDetranVerificadorPontosRepository
+    {
+        Task<IEnumerable<PontoCarteira>> ConsultarPontoCarteira(Carteira carteira);
+    }
